2128aa62607bc03d0352a6b4488f6f11f86a88de
aravind kumar <30lmas09@gmail.com>
2022-11-22 20:23:14 +0530

    Bug Test

--- utils/axios.js
+++ utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://fastdbaccess.vercel.app/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://fastdbaccess.vercel.app/api",
+  // baseURL: "http://localhost:3000/api",
 });
 
 export default instance;
